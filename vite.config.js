80bad42afc6dc0e6f055ff133267f4ff8824178b
RyanHsu1128 <coyte124@gmail.com>
2025-12-16 00:05:43 +0800

    chore: 更新 Vite 配置以優化建構或開發體驗。

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/pixel_game/', // 重要：請將 pixel_game 改成您的 GitHub Repository 名稱
